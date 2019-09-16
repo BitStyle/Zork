0c38aabc88f4373248a828f4a2d385f49ca27684
BitStyle <hwglaspie51@gmail.com>
2019-09-16 14:05:24 -0400

    8 - Object-Oriented Rooms

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,14 +5,29 @@ namespace Zork
 {
     class Program
     {
-        private static readonly string[,] Rooms =
+        private static readonly Room[,] Rooms =
         {
-            {"Rocky Trail", "South of House", "Canyon View" },
-            {"Forest", "West of House", "Behind House" },
-            {"Dense Woods", "North of House", "Clearing" }
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static string CurrentRoom
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
+
+        public static Room CurrentRoom
         {
             get
             {
@@ -25,6 +40,7 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
@@ -32,16 +48,11 @@ namespace Zork
                 Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString;
                 switch (command)
                 {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
 
                     case Commands.LOOK:
-                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -54,11 +65,18 @@ namespace Zork
                         }
                         break;
 
+                    case Commands.QUIT:break;
+
                     default:
                         Console.WriteLine("Unknown command.");
                         break;
                 }
             }
+            if( command == Commands.QUIT)
+            {
+                Console.WriteLine("Thank you for playing!");
+            }
+            
         }
 
         private static bool Move(Commands command)
@@ -100,7 +118,7 @@ namespace Zork
             Commands.NORTH,
             Commands.SOUTH,
             Commands.EAST,
-            Commands.WEST
+            Commands.WEST,
         };
 
     }
