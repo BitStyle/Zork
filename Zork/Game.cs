cb47266c95f0939b568f276c50d41e4d42ab66c8
BitStyle <hwglaspie51@gmail.com>
2019-09-18 15:18:39 -0400

    12 - A Larger World (Partial)

---
+++ Zork/Game.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Zork
+{
+    class Game
+    {
+        public World World { get; }
+
+        [JsonIgnore]
+        public Player Player { get; private set; }
+
+        [JsonIgnore]
+        private bool IsRunning { get; set; }
+
+        public Game(World world, Player player)
+        {
+            World = world;
+            Player = player;
+        }
+
+        public void Run()
+        {
+            IsRunning = true;
+            Room previousRoom = null;
+            while (IsRunning)
+            {
+                Console.WriteLine(Player.Location);
+                if (previousRoom != Player.Location)
+                {
+                    Console.WriteLine(Player.Location.Description);
+                    previousRoom = Player.Location;
+                }
+            }
+        }
+    }
+
+    public static Game Load(string filename)
+    {
+        Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+        return game;
+    }
+}
